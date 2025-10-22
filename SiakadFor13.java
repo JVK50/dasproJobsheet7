20e89ad18af23cd8ad1fa187de6dfc8e71de6d66
JVK50 <jidanali32@gmail.com>
2025-10-22 11:00:06 +0700

    Modifikasi 1

--- SiakadFor13.java
+++ SiakadFor13.java
@@ -7,9 +7,10 @@ public class SiakadFor13 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int jumlahLulus = 0, jumlahTidakLulus = 0;
         
-        for (int i = 1; i <= 10; i++) {
-            System.out.println("Masukkan nilai mahasiswa ke-" + i + ": ");
+        for (int x = 1; x <= 10; x++) {
+            System.out.println("Masukkan nilai mahasiswa ke-" + x + ": ");
             nilai = sc.nextDouble();
             if (nilai > tertinggi) {
                 tertinggi = nilai;
@@ -17,10 +18,17 @@ public class SiakadFor13 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai > 60 ) {
+                jumlahLulus++;
+            } else {
+                jumlahTidakLulus++;
+            }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
-        
+        System.out.println("Jumlah mahasiswa yang lulus: " + jumlahLulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + jumlahTidakLulus);
+
         sc.close();
     }
 }
