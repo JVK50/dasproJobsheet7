3c8f24b9f1ab50bafc8b0ebb8fc8e85f61b5a25b
JVK50 <jidanali32@gmail.com>
2025-10-22 13:39:48 +0700

    Modifikasi no4 Percobaan 1

--- SiakadFor13.java
+++ SiakadFor13.java
@@ -24,6 +24,7 @@ public class SiakadFor13 {
                 jumlahTidakLulus++;
             }
         }
+        
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
         System.out.println("Jumlah mahasiswa yang lulus: " + jumlahLulus);
